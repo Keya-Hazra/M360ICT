282698298dd6ec1187d1b317aebcc57333251725
Keya-Hazra <63946240+Keya-Hazra@users.noreply.github.com>
2022-12-02 11:15:24 +0600

    fetch singleLaunch single launch data fetching done

--- src/pages/SingleLaunch.tsx
+++ src/pages/SingleLaunch.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetLaunchQuery } from "../services/LaunchApi";
 
-const SingleLaunch = () => {
-	return <div>SingleLaunch</div>;
+const SingleLaunch = ()  => {
+	const { flight_number } = useParams();
+
+	const { data } = useGetLaunchQuery(flight_number!);
+	console.log(data);
+	return (
+		<div>
+			<div>SingleLaunch</div>
+			<div>{data?.flight_number}</div>
+		</div>
+	);
 };
 
 export default SingleLaunch;
--- src/services/LaunchApi.tsx
+++ src/services/LaunchApi.tsx
@@ -2,13 +2,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { LaunchType } from "../types";
 
 export const LaunchApi = createApi({
-    reducerPath:'LaunchApi',
+	reducerPath: "LaunchApi",
 	baseQuery: fetchBaseQuery({ baseUrl: "https://api.spacexdata.com/v3" }),
+	tagTypes: ["launch"],
 	endpoints: (builder) => ({
-		getLaunch: builder.query<LaunchType[], void>({
-			query: () => "/launches",
+		getLaunches: builder.query<LaunchType[], void>({
+			query: () => `/launches`,
+			providesTags: ["launch"],
+		}),
+		getLaunch: builder.query<LaunchType, string>({
+			query: (flight_number) => `/launches/${flight_number}`,
+			providesTags: ["launch"],
 		}),
 	}),
 });
 
-export const { useGetLaunchQuery } = LaunchApi;
+export const { useGetLaunchesQuery, useGetLaunchQuery } = LaunchApi;
