1de875fd034ef2f0dc10b392da7c263af718a918
Keya-Hazra <63946240+Keya-Hazra@users.noreply.github.com>
2022-12-02 11:14:07 +0600

    search search and data fetching done

--- src/pages/Launch.tsx
+++ src/pages/Launch.tsx
@@ -1,20 +1,51 @@
-import React from "react";
-import { useGetLaunchQuery } from "../services/LaunchApi";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useGetLaunchesQuery } from "../services/LaunchApi";
+import { LaunchType } from "../types";
 
 const Launch = () => {
-	const { data, error, isLoading, isFetching, isSuccess } = useGetLaunchQuery();
+	const { data, error, isLoading, isFetching, isSuccess } =
+		useGetLaunchesQuery();
+	console.log(data);
+	const [launchList, setLaunchList] = useState(data);
+	console.log(launchList);
+
+	const [searchItem, setSearchItem] = useState("");
 	return (
 		<div>
-		
+			<input
+				type="text"
+				// value={text}
+				placeholder="search"
+				onChange={(e) => setSearchItem(e.target.value)}
+			/>
+
 			{isSuccess && (
 				<div>
-					{data?.map((launch) => {
-						return (
-							<div key={launch.flight_number}>
-								<div>{launch.mission_name}</div>
-							</div>
-						);
-					})}
+					{data
+						?.filter((val) => {
+							if (searchItem === "") {
+								return val;
+							} else if (
+								val.rocket.rocket_name
+									.toLowerCase()
+									.includes(searchItem.toLowerCase())
+							) {
+								return val;
+							}
+						})
+						.map((val: any, index: any) => {
+							return (
+								<div key={index}>
+									<div>
+										<Link to={`/launches/${val.flight_number}`}>
+											{val.flight_number}
+										</Link>
+										{val.mission_name} {val.rocket.rocket_name}
+									</div>
+								</div>
+							);
+						})}
 				</div>
 			)}
 		</div>
